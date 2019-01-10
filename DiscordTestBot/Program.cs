63b127cbb601940f2864bf41c55cc9a30dd015d7
Alex Kalman <Alexkalman123@gmail.com>
2019-01-10 09:53:45 -0600

    Updated usings

--- DiscordTestBot/Program.cs
+++ DiscordTestBot/Program.cs
@@ -1,9 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
 namespace DiscordTestBot
 {
     class Program
